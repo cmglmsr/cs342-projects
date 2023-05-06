6b83b2bff160f8f7705ad7e08e6206c9527f11ef
cmglmsr <mgulumser@my.aci.k12.tr>
2023-05-06 22:56:27 +0300

    done except detection and print

--- p3/22002379-22003430/rm.c
+++ p3/22002379-22003430/rm.c
@@ -26,6 +26,62 @@ pthread_mutex_t mutex;
 pthread_cond_t* cvs;
 
 // end of global variables
+void printMat(int** mat, int n, int m) {
+    for(int i = 0; i < n; i++) {
+        printf("\n");
+        for(int j = 0; j < m; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Return true if arr1 <= arr2, false otherwise
+bool arrLessThan(int* arr1, int* arr2, int N) {
+    for(int i = 0; i < N; i++) {
+        if(arr1[i] > arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Check if the current state is safe
+bool checkSafe(int* available, int** need, int** allocation) {
+    int work[M];
+    bool finish[N];
+    for(int i = 0; i < M; i++)
+        work[i] = available[i];
+    for(int i = 0; i < N; i++)
+        finish[i] = 0;
+
+    while(1) {
+        int x = -1;
+        for(int i = 0; i < N; i++) {
+            if(!finish[i] && arrLessThan(need[i], work, M)) {x=i; break;}
+        }
+        if(x==-1) break;
+        else {
+            for(int i = 0; i < M; i++) {
+                work[i] += allocation[x][i];
+                finish[x] = true;
+            }
+        }
+    }
+    for(int i = 0; i < N; i++) {
+        if(!finish[i]) return false;
+    }
+    return true;
+}
+
+bool checkAvailability(int request[]) {
+    for(int i = 0; i < M; i++) {
+        if(request[i] > available[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 /**
  * @brief This function will be called by a thread immediately after it starts
@@ -177,34 +233,45 @@ int rm_request (int request[])
         pthread_cond_wait(&cvs[tid], &mutex);
     }
 
-    // Compute the new state
-    int* newAvailable = (int*)malloc(sizeof(int)*M); 
-    int** newAllocation = (int**)malloc(sizeof(int*)*N);
-    int** newNeed = (int**)malloc(sizeof(int*)*N);
-    for(int i = 0; i < M; i++) {
-        newAvailable[i] = available[i] - request[i];
-    }
-    for(int i = 0; i < N; i++) {
-        newAllocation[i] = (int*)malloc(sizeof(int)*M);
-        newNeed[i] = (int*)malloc(sizeof(int)*M);
-        for(int j = 0; j < M; j++) {
-            if(i == tid) {
-                newAllocation[i][j] = allocation[i][j] + request[i];
-                newNeed[i][j] = need[i][j] - request[i];
-            }
-            else {
-                newAllocation[i][j] = allocation[i][j];
-                newNeed[i][j] = need[i][j];
+    // Check if the new state is safe
+    while(1) {
+        // Compute the new state
+        int* newAvailable = (int*)malloc(sizeof(int)*M); 
+        int** newAllocation = (int**)malloc(sizeof(int*)*N);
+        int** newNeed = (int**)malloc(sizeof(int*)*N);
+        for(int i = 0; i < M; i++) {
+            newAvailable[i] = available[i] - request[i];
+        }
+        for(int i = 0; i < N; i++) {
+            newAllocation[i] = (int*)malloc(sizeof(int)*M);
+            newNeed[i] = (int*)malloc(sizeof(int)*M);
+            for(int j = 0; j < M; j++) {
+                if(i == tid) {
+                    newAllocation[i][j] = allocation[i][j] + request[i];
+                    newNeed[i][j] = need[i][j] - request[i];
+                }
+                else {
+                    newAllocation[i][j] = allocation[i][j];
+                    newNeed[i][j] = need[i][j];
+                }
             }
         }
+        if(checkSafe(newAvailable, newNeed, newAllocation)) {
+            free(available);
+            for(int i = 0; i < N; i++) { 
+                free(allocation[i]);
+                free(need[i]);
+            } 
+            available = newAvailable;
+            need = newNeed;
+            allocation = newAllocation;
+            pthread_mutex_unlock(&mutex);
+            return 0;
+        }
+        else {
+            pthread_cond_wait(&cvs[tid], &mutex);
+        }
     }
-
-    // Check if the new state is safe
-    if(checkSafe(newAvailable, newNeed, newAllocation)) {
-
-    }
-    else { }
-    
     pthread_mutex_unlock(&mutex);
     return 0;
 }
@@ -225,9 +292,46 @@ int rm_request (int request[])
 */
 int rm_release (int release[])
 {
-    int ret = 0;
+    pthread_t tid = pthread_self();
 
-    return (ret);
+    // Check error condition
+    for(int i = 0; i < M; i++) {
+        if(release[i] > allocation[tid][i])
+            return -1;
+    }
+
+    // Set the new matrices & vectors
+    int* newAvailable = (int*)malloc(sizeof(int)*M); 
+    int** newAllocation = (int**)malloc(sizeof(int*)*N);
+    int** newNeed = (int**)malloc(sizeof(int*)*N);
+
+    pthread_mutex_lock(&mutex);
+    for(int i = 0; i < M; i++) {
+        newAvailable[i] = available[i] + release[i];
+    }
+    for(int i = 0; i < N; i++) {
+        newAllocation[i] = (int*)malloc(sizeof(int)*M);
+        newNeed[i] = (int*)malloc(sizeof(int)*M);
+        for(int j = 0; j < M; j++) {
+            if(i == tid) {
+                newAllocation[i][j] = allocation[i][j] - release[i];
+                newNeed[i][j] = need[i][j] + release[i];
+            } else {
+                newAllocation[i][j] = allocation[i][j];
+                newNeed[i][j] = need[i][j];
+            }
+        }
+    }
+    free(available);
+    for(int i = 0; i < N; i++) { 
+        free(allocation[i]);
+        free(need[i]);
+    } 
+    available = newAvailable;
+    need = newNeed;
+    allocation = newAllocation;
+    pthread_mutex_unlock(&mutex);
+    return 0;
 }
 
 /**
@@ -257,63 +361,6 @@ int rm_detection()
 void rm_print_state (char hmsg[])
 {
     return;
-}
-
-void printMat(int** mat, int n, int m) {
-    for(int i = 0; i < n; i++) {
-        printf("\n");
-        for(int j = 0; j < m; j++) {
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// Return true if arr1 <= arr2, false otherwise
-bool arrLessThan(int* arr1, int* arr2, int N) {
-    for(int i = 0; i < N; i++) {
-        if(arr1[i] > arr2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Check if the current state is safe
-bool checkSafe(int* available, int** need, int** allocation) {
-    int work[M];
-    bool finish[N];
-    for(int i = 0; i < M; i++)
-        work[i] = available[i];
-    for(int i = 0; i < N; i++)
-        finish[i] = 0;
-
-    while(1) {
-        int x = -1;
-        for(int i = 0; i < N; i++) {
-            if(!finish[i] && arrLessThan(need[i], work, M)) {x=i; break;}
-        }
-        if(x==-1) break;
-        else {
-            for(int i = 0; i < M; i++) {
-                work[i] += allocation[x][i];
-                finish[x] = true;
-            }
-        }
-    }
-    for(int i = 0; i < N; i++) {
-        if(!finish[i]) return false;
-    }
-    return true;
-}
-
-bool checkAvailability(int request[]) {
-    for(int i = 0; i < M; i++) {
-        if(request[i] > available[i]) {
-            return false;
-        }
-    }
-    return true;
 }
 
 int main() {
